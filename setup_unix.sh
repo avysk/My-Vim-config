b53291396af5b891d27c3c8f13450bbcda2b401a
Alexey Vyskubov <alexey@hotmail.fi>
2017-11-02 16:40:40 +0200

    better install instructions

--- setup_unix.sh
+++ setup_unix.sh
@@ -8,8 +8,11 @@ fi
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/gvimrc ~/.gvimrc
 
-echo "Run git submodule init; git submodule update"
+echo "Run git submodule update --init --recursive"
 echo "Then run make in ~/.vim/bundle/vimproc, in ~/.vim/bundle/lusty"
 echo "and ~/.vim/bundle/vim-fsharp"
 echo "Then go to ~/.vim/bundle/omnisharp-vim; do there"
-echo "'git submodule update --init --recursive && cd server && xbuild'"
+echo "'cd server && xbuild'"
+echo "Then go to ~/.vim/bundle/YouCompleteMe; do there"
+echo "'python install.py --all'"
+echo "Then run vim and execute ':Helptags'"
